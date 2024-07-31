59b95aef5a3c2f8e19abef8ad0ccb775668188f9
Patrick Marchwiak <pmarchwiak@gmail.com>
2024-07-31 15:32:16 -0700

    Add tab context menu, support moving to existing window, add icon

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# Tabs to Window
+![alt text](icon-48.png "icon")
+
+# Tabs to Window 
 
 This is a Firefox extension that enables easy movement of tabs to another window.
 
 ## TODO
-[ ] Support moving tabs to an existing window
-[ ] Use tab right-click menu instead of a popup menu
-[ ] Support moving tabs to left of active tab
-[ ] Cleanup unused example code
-[ ] Find an icon
-[ ] Publish the extension
+- [x] Support moving tabs to an existing window
+- [x] Use tab right-click menu instead of a popup menu
+- [x] Support moving tabs to left of active tab
+- [ ] Look into creating submenus for left/right/all tabs
+- [ ] Cleanup unused example code
+- [ ] Check pinned tab handling
+- [x] Find an icon
+- [ ] Internationalize
+- [ ] Publish the extension
 
 Originally based on Mozilla's https://github.com/mdn/webextensions-examples/tree/main/tabs-tabs-tabs
 example.
--- background.js
+++ background.js
@@ -1,37 +1,123 @@
-function updateCount(tabId, isOnRemoved) {
-  browser.tabs.query({})
-  .then((tabs) => {
-    let length = tabs.length;
+function onMenuCreated() {
+  console.log(`menu created`);
+}
 
-    // onRemoved fires too early and the count is one too many.
-    // see https://bugzilla.mozilla.org/show_bug.cgi?id=1396758
-    if (isOnRemoved && tabId && tabs.map((t) => { return t.id; }).includes(tabId)) {
-      length--;
-    }
+function refreshTabMenu(details) {
+  browser.menus.create(
+    {
+      id: "left-new",
+      title: "Move Tabs to Left to New Window",
+      // title: browser.i18n.getMessage("menuItemRemoveMe"),
+      contexts: ["tab"],
+    },
+    onMenuCreated,
+  );
 
-    browser.browserAction.setBadgeText({text: length.toString()});
-    if (length > 2) {
-      browser.browserAction.setBadgeBackgroundColor({'color': 'green'});
-    } else {
-      browser.browserAction.setBadgeBackgroundColor({'color': 'red'});
-    }
-  });
+  browser.menus.create(
+    {
+      id: "right-new",
+      title: "Move Tabs to Right to New Window",
+      // title: browser.i18n.getMessage("menuItemRemoveMe"),
+      contexts: ["tab"],
+    },
+    onMenuCreated,
+  );
+
+  browser.windows.getAll({populate: false, windowTypes: ["normal"],})
+    .then(
+      (windows) => {
+        let nonFocusedWindows = windows.filter((window) => !window.focused);
+        console.log(`found ${nonFocusedWindows.length} non focused windows`);
+        for (const window of nonFocusedWindows) {
+          browser.menus.create(
+            {
+              id: `right-window-${window.id}`,
+              title: `Move Tabs to Right to Window "${window.title}"`,
+              contexts: ["tab"],
+            }
+          );
+        }
+      }, 
+      (error) => console.log(`failed to get windows during menu creation`)
+    );
 }
 
 function createWindowAndMoveTabs(tabIds) {
   console.log(`creating a new window and moving tabs`);
-  let creating = browser.windows.create();
+  let firstTabId = tabIds[0];
+  let creating = browser.windows.create({tabId: firstTabId});
 
   creating.then(
     (windowInfo) => {
-      console.log(`created new window`);
-      console.log(`now moving tab ids ${tabIds}`);
-      let moving = browser.tabs.move(
-        tabIds, {windowId: windowInfo.id, index: -1}
-      );
+      console.log(`created new window with ${firstTabId}`);
+      
+      if (tabIds.length > 1) {
+        let otherTabIds = tabIds.slice(1);
+        console.log(`now moving other tab ids ${otherTabIds}`);
+        let moving = browser.tabs.move(
+          otherTabIds, {windowId: windowInfo.id, index: -1}
+        );
+      }
     },
     (error) => console.log(`error: ${error}`));
+}
 
+function moveTabsToWindow(tabIds, windowId) {
+  browser.tabs.move(
+    tabIds, {windowId, index: -1}
+  ); 
+}
+
+function onMenuItemClicked(menusOnClickData) {
+  console.log(`menu item clicked: ${menusOnClickData.menuItemId}, ${JSON.stringify(menusOnClickData)}`)
+  let menuItemId = menusOnClickData.menuItemId;
+  let pageUrl = menusOnClickData.pageUrl;
+  let querying = browser.tabs.query({currentWindow: true});
+  let tabIds = [];
+
+  // Check if we're moving to an existing window
+  let targetWindowId;
+  if (menuItemId.startsWith('right-window')) {
+    // Match one or more digits at the end of the string
+    const idMatch = menuItemId.match(/\d+$/);
+    if (idMatch) {
+      targetWindowId = parseInt(idMatch[0], 10);
+    }
+  }
+
+  querying.then(
+    (tabs) => {
+      console.log(`found ${tabs.length} tabs in window`);
+
+      // is there a better way to identify which tab had it's menu clicked??
+      // this method won't work well if there are multiple duplicate tabs open..
+      let clickedTab = tabs.find((tab) => tab.url === pageUrl);
+      console.log(`found clicked tab ${clickedTab.id}`)
+
+      let tabsToMove;
+      if (menuItemId.startsWith("right")) {
+        tabsToMove = tabs.filter((tab) => tab.index > clickedTab.index);
+      }
+      else if (menuItemId.startsWith("left")) {
+        tabsToMove = tabs.filter((tab) => tab.index < clickedTab.index);
+      }
+
+      if (tabsToMove) {
+        console.log(`found ${tabsToMove.length} tabs to move`);
+        tabIds = tabsToMove.map((tab) => tab.id);
+      }
+      console.log(`tab ids: ${JSON.stringify(tabIds)}`);
+
+      if (tabIds && tabIds.length > 0) {
+        if (targetWindowId) {
+          moveTabsToWindow(tabIds, targetWindowId)
+        }
+        else {
+          createWindowAndMoveTabs(tabIds);
+        }
+      }
+    }
+  );
 }
 
 function handleMessage(request, sender, sendResponse) {
@@ -42,11 +128,9 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
+browser.menus.onClicked.addListener(onMenuItemClicked);
 
-browser.tabs.onRemoved.addListener(
-  (tabId) => { updateCount(tabId, true);
-});
-browser.tabs.onCreated.addListener(
-  (tabId) => { updateCount(tabId, false);
-});
-updateCount();
+browser.runtime.onInstalled.addListener(refreshTabMenu);
+browser.windows.onCreated.addListener(refreshTabMenu);
+browser.windows.onFocusChanged.addListener(refreshTabMenu);
+browser.windows.onRemoved.addListener(refreshTabMenu);
